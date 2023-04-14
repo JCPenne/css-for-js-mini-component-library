8dfabd2e178a49cf0c5f5e4728090fb35f6f2461
JCPenne <jackcpalmer@icloud.com>
2023-04-13 20:56:53 -0700

    finish initial attempt at progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,60 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = value === 100 ? { 'border-radius': 4 + 'px' } : {};
+
+  let Component;
+  if (size === 'large') {
+    Component = LargeOuterBar;
+  } else if (size === 'medium') {
+    Component = MediumOuterBar;
+  } else if (size === 'small') {
+    Component = SmallOuterBar;
+  } else {
+    throw new Error(`Unrecognized ProgressBar size: ${size} Accepted sizes: small, medium, large`);
+  }
+
+  return (
+    <>
+      <Component
+        role='progressbar'
+        aria-valuenow={value}
+        aria-valuemin='0'
+        aria-valuemax='100'
+      >
+        <VisuallyHidden>{value}%</VisuallyHidden>
+        <Inner value={value} style={styles} />
+      </Component>
+    </>
+  );
 };
+
+const OuterBase = styled.div`
+  position: relative;
+  width: 100%;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+`;
+
+const LargeOuterBar = styled(OuterBase)`
+  height: 24px;
+  padding: Outer4px;
+  border-radius: 8px;
+`;
+const MediumOuterBar = styled(OuterBase)`
+  height: 12px;
+`;
+const SmallOuterBar = styled(OuterBase)`
+  height: 8px;
+`;
+
+const Inner = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${p => p.value}%;
+  height: 100%;
+  position: relative;
+  border-radius: 4px 0px 0px 4px;
+`;
 
 export default ProgressBar;
